52383a5d603e16bf1c98ed6a049a153c318d28d8
Raphaël DUCOM <raphael.ducom@gmail.com>
2019-01-30 15:36:21 +0100

    project bootstrap

---
+++ Benchmark/Benchmark/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<CompressionBenchmark>();
+        }
+    }
+
+    public class CompressionBenchmark
+    {
+        [Benchmark]
+        public void MessagePack()
+        {
+
+        }
+
+        [Benchmark]
+        public void Zero() 
+
+        {
+
+        }
+    }
+}
